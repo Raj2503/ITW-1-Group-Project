f40f6f9d244a59c30592ef31ae3969852ea51a5d
Raj Aryan <raj250301@gmail.com>
2020-11-05 16:07:37 +0530

    Added functionality to change sheets
    Co-Authored-By: Sarthak Gupta <63359447+SarthakSavasil@users.noreply.github.com>

--- project.py
+++ project.py
@@ -1,10 +1,15 @@
 from tkinter import *
 from PIL import ImageTk,Image
 import pandas as pd
-xls = pd.ExcelFile('Attendance_Data.xlsx')
-df1 = pd.read_excel(xls, 'Sheet1',index_col = None)
 
-root = Tk() 
+df = pd.ExcelFile('output.xlsx')
+Sheet_names_list = df.sheet_names
+dflist = []
+print(Sheet_names_list)
+for sheet in Sheet_names_list :
+   dflist.append(df.parse(sheet_name=sheet,index_col=None))
+
+root = Tk()
 root.title("Attendance Record & Management")
 root.geometry("400x300")
 frame = LabelFrame(root, text= "Welcome to Attendance Manager",padx=100, pady=80)
@@ -13,12 +18,10 @@ frame.pack(fill=BOTH,padx=10,pady=10)
 classroom = NONE
 Present = []
 Absent = []
-DayN = 0
-for i in range(1,41):
-    if df1['Day'+str(i)].isnull().sum()==5:
-        DayN =i-1
-        break
+DayN = 1
+
 #print("\n\n" + str(DayN) + "\n\n\n")
+
 def show():
     global DayN
     myLabel = Label(root,text=clicked.get()).pack()
@@ -27,17 +30,22 @@ def onClick(idx):
     print(idx) # Print the index value
  #   print(Attended[idx].cget("text"))
 
-def markpresent(x):
+def markpresent(idx,x):
     global DayN
-    df1['Day'+str(int(DayN))].iloc[x] = Present[x].cget("text")
+    dflist[x]['Day'+str(int(DayN))].iloc[idx] = Present[idx].cget("text")
     #print(df1)
-    df1.to_excel('Attendance_Data.xlsx',index = False)
 
-def markabsent(x):
+
+def markabsent(idx,x):
     global DayN
-    df1['Day'+str(int(DayN))].iloc[x] = Absent[x].cget("text")
+    dflist[x]['Day'+str(int(DayN))].iloc[idx] = Absent[idx].cget("text")
     #print(df1)
-    df1.to_excel('Attendance_Data.xlsx',index = False)
+
+def save():
+    with pd.ExcelWriter('output.xlsx') as writer:
+        for i in range(7):
+            dflist[i].to_excel(writer,sheet_name="Sheet_name_" + str(i+1),index=False)
+
 
 def percentage():
     root.withdraw()
@@ -47,11 +55,23 @@ def percentage():
     frame = LabelFrame(record, text= "Mark Attendance",padx=50,pady=40)
     frame.grid(padx=10,pady=10)
     MyLabel1 = Label(frame,text="Welcome to Attendance Record for " + clicked.get()).grid(row=0,columnspan=4,padx=10)
-
+    x = 0
+    if clicked.get()=='Course1':
+        x=0
+    if clicked.get()=='Course2':
+        x=1
+    if clicked.get()=='Course3':
+        x=2
+    if clicked.get()=='Course4':
+        x=3
+    if clicked.get()=='Course5':
+        x=4
+    if clicked.get()=='Course6':
+        x=5
     for i in range(5):
-        name = Label(frame,text=df1['Name'][i]).grid(row=i+1,column=0)
-        attended = (df1.iloc[i,4:]=="Present").sum()
-        total = 40 - (df1.iloc[1,4:].isnull()).sum()
+        name = Label(frame,text=dflist[x]['Name'][i]).grid(row=i+1,column=0)
+        attended = (dflist[x].iloc[i,4:]=="Present").sum()
+        total = 40 - (dflist[x].iloc[1,4:].isnull()).sum()
         if (attended/total)*100 < 75:
             percent = Label(frame,text = "{:.2f}".format((attended/total)*100) +" %", fg = "red",font = "Times").grid(row=i+1,column=1)
         else:
@@ -65,8 +85,24 @@ def open():
     global Present
     global Absent
     global DayN
-    DayN= int(DayN) + 1
-
+    x = 0
+    if clicked.get()=='Course1':
+        x=0
+    if clicked.get()=='Course2':
+        x=1
+    if clicked.get()=='Course3':
+        x=2
+    if clicked.get()=='Course4':
+        x=3
+    if clicked.get()=='Course5':
+        x=4
+    if clicked.get()=='Course6':
+        x=5
+    
+    for i in range(1,41):
+        if dflist[x]['Day'+str(i)].isnull().sum()==5:
+            DayN =i
+            break
 
     classroom = Toplevel()
     classroom.title(clicked.get())
@@ -79,28 +115,32 @@ def open():
 
 
     for i in range(5):
-        name = Label(frame,text=df1['Name'][i]).grid(row=i+3,column=0)
-        c = Checkbutton(frame,text="Present",variable=Present,onvalue=1,offvalue=0,command = lambda idx = i: markpresent(idx))
-        d = Checkbutton(frame,text="Absent",variable=Absent,onvalue=0,offvalue=1,command = lambda idx = i: markabsent(idx))
+        name = Label(frame,text=dflist[x]['Name'][i]).grid(row=i+3,column=0)
+        c = Checkbutton(frame,text="Present",variable=Present,onvalue=1,offvalue=0,command = lambda idx = i: markpresent(idx,x))
+        d = Checkbutton(frame,text="Absent",variable=Absent,onvalue=0,offvalue=1,command = lambda idx = i: markabsent(idx,x))
         c.grid(row=i+3,column=1)
         d.grid(row=i+3,column=2)
         
         Present.append(c)
         Absent.append(d)
-    btn2 = Button(frame,text="Close Window",command=lambda:[classroom.withdraw(),root.deiconify()]).grid(row=11,column=0,ipadx=20,pady=30)
+    btn2 = Button(frame,text="Close Window",command=lambda:[classroom.withdraw(),root.deiconify(),save()]).grid(row=11,column=0,ipadx=20,pady=30)
     
 
 
 options = [
     "Course1",
-    "Course2"
+    "Course2",
+    "Course3",
+    "Course4",
+    "Course5",
+    "Course6",
 ]
 
 clicked = StringVar()
 clicked.set("Select one from below")
 drop = OptionMenu(frame, clicked, *options).grid(row=1,column=2,columnspan=2)
 
-btn = Button(frame,text="Mark Attendance",command=open).grid(row=2,column=2,columnspan=2,pady=10)
-btn2 = Button(frame,text="Show Attendance Percentage",command=percentage).grid(row=3,column=2,columnspan=2,pady=10)
+btn = Button(frame,text="Mark Attendance",command=lambda:[open()]).grid(row=2,column=2,columnspan=2,pady=10)
+btn2 = Button(frame,text="Show Attendance Percentage",command=lambda:[percentage()]).grid(row=3,column=2,columnspan=2,pady=10)
 
 root.mainloop()
