3e5f509e9185663de1486c9837fd3d847bf4cf02
Raj Aryan <raj250301@gmail.com>
2020-11-05 10:06:35 +0530

    Added different colors for different %s

--- project.py
+++ project.py
@@ -15,7 +15,7 @@ for i in range(1,41):
     if df1['Day'+str(i)].isnull().sum()==5:
         DayN =i-1
         break
-print("\n\n" + str(DayN) + "\n\n\n")
+#print("\n\n" + str(DayN) + "\n\n\n")
 def show():
     global DayN
     myLabel = Label(root,text=clicked.get()).pack()
@@ -47,7 +47,10 @@ def percentage():
         name = Label(record,text=df1['Name'][i]).grid(row=i+1,column=0)
         attended = (df1.iloc[i,4:]=="Present").sum()
         total = 40 - (df1.iloc[1,4:].isnull()).sum()
-        percent = Label(record,text = (attended/total)*100).grid(row=i+1,column=1)
+        if (attended/total)*100 < 75:
+            percent = Label(record,text = "{:.2f}".format((attended/total)*100) +" %", fg = "red",font = "Times").grid(row=i+1,column=1)
+        else:
+            percent = Label(record,text = "{:.2f}".format((attended/total)*100) + " %", fg = "dark green",font = "Times").grid(row=i+1,column=1)
     btn2 = Button(record,text="Close Window",command=lambda:[record.withdraw(),root.deiconify()]).grid(row=11)    
 
 
